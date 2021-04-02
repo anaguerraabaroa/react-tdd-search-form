0cf51cadbb36e1e4871c068ee1fe4dda9a84befa
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-02 14:19:04 +0100

    Add test to validate that GithubSearchComponent is rendered and it has a title

---
+++ src/components/github-search-page/github-search-page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export const GithubSearchPage = () => (
+  <Typography variant="h3" component="h1">
+    Github repositories list
+  </Typography>
+)
+
+export default GithubSearchPage
---
+++ src/components/github-search-page/github-search-page.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+
+import {GithubSearchPage} from './github-search-page'
+
+describe('when the GithubSearchPage is mounted', () => {
+  // test page title
+  it('must display the title', () => {
+    render(<GithubSearchPage />)
+    
+    expect(
+      screen.getByRole('heading', {name: /github repositories list/i}),
+    ).toBeInTheDocument()
+  })
+
+
