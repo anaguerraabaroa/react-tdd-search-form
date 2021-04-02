97be71f6dcbfcd3f41f815756abc4dbf9fc87acc
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-02 16:11:26 +0100

    Add test to validate that the search button is rendered

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 export const GithubSearchPage = () => (
   <>
     <Typography variant="h3" component="h1">
       Github repositories list
     </Typography>
+
     <TextField label="Filter by" id="filterby"></TextField>
+
+    <Button>Search</Button>
   </>
 )
 
--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -18,4 +18,9 @@ describe('when the GithubSearchPage is mounted', () => {
   it('must be an input text with label "filter by" field', () => {
     expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument()
   })
+
+  // test search button
+  it('must be a search button', () => {
+    expect(screen.getByRole('button', {name: /search/i})).toBeInTheDocument()
+  })
 })
