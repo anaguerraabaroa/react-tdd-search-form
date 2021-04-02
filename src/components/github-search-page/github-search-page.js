8f195a4768e3bab6cb3c23b62b58984609d6704c
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-02 16:05:07 +0100

    Add test to validate that the label and input text are rendered

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
 
 export const GithubSearchPage = () => (
-  <Typography variant="h3" component="h1">
-    Github repositories list
-  </Typography>
+  <>
+    <Typography variant="h3" component="h1">
+      Github repositories list
+    </Typography>
+    <TextField label="Filter by" id="filterby"></TextField>
+  </>
 )
 
 export default GithubSearchPage
--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -4,13 +4,18 @@ import {render, screen} from '@testing-library/react'
 import {GithubSearchPage} from './github-search-page'
 
 describe('when the GithubSearchPage is mounted', () => {
+  // render the component before each test
+  beforeEach(() => render(<GithubSearchPage />))
+
   // test page title
   it('must display the title', () => {
-    render(<GithubSearchPage />)
-    
     expect(
       screen.getByRole('heading', {name: /github repositories list/i}),
     ).toBeInTheDocument()
   })
 
-
+  // test input text + label
+  it('must be an input text with label "filter by" field', () => {
+    expect(screen.getByLabelText(/filter by/i)).toBeInTheDocument()
+  })
+})
