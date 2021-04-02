9130c6cd6244bf23873ca282ae9b2e8a9f6179c9
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-02 17:14:16 +0100

    Add test to validate that when the button is clicked, it is disabled until search is done

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
@@ -6,35 +6,54 @@ import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 
-export const GithubSearchPage = () => (
-  <Container>
-    <Typography variant="h3" component="h1">
-      Github repositories list
-    </Typography>
+export const GithubSearchPage = () => {
+  // state
+  const [isSearching, setIsSearching] = useState(false)
 
-    <Grid container spacing={2} justify="space-between">
-      <Grid item md={6} xs={12}>
-        <TextField fullWidth label="Filter by" id="filterby"></TextField>
-      </Grid>
+  // event click handler
+  // when button receives the event, an API request is sent (async)
+  const handleClick = async () => {
+    setIsSearching(true)
+    await Promise.resolve()
+    setIsSearching(false)
+  }
 
-      <Grid item md={3} xs={12}>
-        <Button fullWidth color="primary" variant="contained">
-          Search
-        </Button>
-      </Grid>
-    </Grid>
-
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      height={400}
-    >
-      <Typography>
-        Please provide a search option and click in the search button
+  return (
+    <Container>
+      <Typography variant="h3" component="h1">
+        Github repositories list
       </Typography>
-    </Box>
-  </Container>
-)
+
+      <Grid container spacing={2} justify="space-between">
+        <Grid item md={6} xs={12}>
+          <TextField fullWidth label="Filter by" id="filterby"></TextField>
+        </Grid>
+
+        <Grid item md={3} xs={12}>
+          <Button
+            fullWidth
+            color="primary"
+            variant="contained"
+            disabled={isSearching}
+            onClick={handleClick}
+          >
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height={400}
+      >
+        <Typography>
+          Please provide a search option and click in the search button
+        </Typography>
+      </Box>
+    </Container>
+  )
+}
 
 export default GithubSearchPage
--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent, waitFor} from '@testing-library/react'
 
 import {GithubSearchPage} from './github-search-page'
 
-describe('when the GithubSearchPage is mounted', () => {
-  // render the component before each test
-  beforeEach(() => render(<GithubSearchPage />))
+// render the component before each test
+beforeEach(() => render(<GithubSearchPage />))
 
+describe('when the GithubSearchPage is mounted', () => {
   // test page title
   it('must display the title', () => {
     expect(
@@ -31,5 +31,24 @@ describe('when the GithubSearchPage is mounted', () => {
         /please provide a search option and click in the search button/i,
       ),
     ).toBeInTheDocument()
+  })
+})
+
+describe('when the user does a search', () => {
+  // test disabled search button
+  it('the search button should be disabled until the search is done', async () => {
+    const button = screen.getByRole('button', {name: /search/i})
+
+    // before the event the button should not be disabled
+    expect(button).not.toBeDisabled()
+
+    // event
+    fireEvent.click(button)
+
+    // after the event the button should be disabled until the search is done
+    expect(button).toBeDisabled()
+
+    // once the search is done the button should not be disabled
+    await waitFor(() => expect(button).not.toBeDisabled())
   })
 })
