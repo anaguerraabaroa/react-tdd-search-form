266eeb426125601eb14f6a542fe0125c982680f7
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-02 19:44:19 +0100

    Create content component and migrate content function

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -5,14 +5,8 @@ import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Avatar from '@material-ui/core/Avatar'
-import Link from '@material-ui/core/Link'
+
+import {Content} from '../content/index'
 
 export const GithubSearchPage = () => {
   // state
@@ -30,52 +24,13 @@ export const GithubSearchPage = () => {
     setIsSearching(false)
   }
 
-  // render content: initial message or table
-  const renderContent = () =>
-    isSearchApplied ? (
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Repository</TableCell>
-              <TableCell>Stars</TableCell>
-              <TableCell>Forks</TableCell>
-              <TableCell>Open issues</TableCell>
-              <TableCell>Updated at</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                <Avatar alt="test" src="/logo192.png" />
-                <Link href="http://localhost:3000/test">Test</Link>
-              </TableCell>
-              <TableCell>10</TableCell>
-              <TableCell>5</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>2021-04-02</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    ) : (
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        height={400}
-      >
-        <Typography>
-          Please provide a search option and click in the search button
-        </Typography>
-      </Box>
-    )
-
   return (
     <Container>
-      <Typography variant="h3" component="h1">
-        Github repositories list
-      </Typography>
+      <Box my={4}>
+        <Typography variant="h3" component="h1">
+          Github repositories list
+        </Typography>
+      </Box>
 
       <Grid container spacing={2} justify="space-between">
         <Grid item md={6} xs={12}>
@@ -95,7 +50,9 @@ export const GithubSearchPage = () => {
         </Grid>
       </Grid>
 
-      {renderContent()}
+      <Box my={4}>
+        <Content isSearchApplied={isSearchApplied} />
+      </Box>
     </Container>
   )
 }
