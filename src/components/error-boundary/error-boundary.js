1b72960fb095a8c169be2ed6dd4fa57086e410f0
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-08 20:23:48 +0100

    Add tests to validate error boundaries

--- src/components/error-boundary/error-boundary.js
+++ src/components/error-boundary/error-boundary.js
@@ -1,7 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const ErrorBoundary = ({children}) => children
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hasError: false}
+  }
+
+  // lifecycle that stays in caché when the error happens
+  static getDerivedStateFromError() {
+    return {hasError: true}
+  }
+
+  // event handler
+  handleReloadClick = () => window.location.reload()
+
+  render() {
+    const {children} = this.props
+    const {hasError} = this.state
+    if (hasError) {
+      return (
+        <>
+          <p>There is an unexpected error</p>
+          <button type="button" onClick={this.handleReloadClick}>
+            Reload
+          </button>
+        </>
+      )
+    }
+    return children
+  }
+}
 
 ErrorBoundary.propTypes = {
   children: PropTypes.node.isRequired,
--- src/components/error-boundary/error-boundary.test.js
+++ src/components/error-boundary/error-boundary.test.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import {screen, render} from '@testing-library/react'
+import {screen, render, fireEvent} from '@testing-library/react'
 
 import {ErrorBoundary} from './error-boundary'
 
-describe.only('when the component works without errors', () => {
+// solution to test error boundaries
+jest.spyOn(console, 'error')
+
+// component
+const ThrowError = () => {
+  throw new Error('ups')
+}
+
+describe('when the component works without errors', () => {
+  // test to validate that component renders
   it('must render the component content', () => {
     render(
       <ErrorBoundary>
@@ -15,10 +24,45 @@ describe.only('when the component works without errors', () => {
   })
 })
 
-describe('when the component throws an errors', () => {
-  it("must render the message 'There is an unexpected error' and a reload button", () => {})
+describe('when the component throws an error', () => {
+  // test to validate error when component renders and reload button
+  it("must render the message 'There is an unexpected error' and a reload button", () => {
+    // render component
+    render(
+      <ErrorBoundary>
+        <ThrowError />
+      </ErrorBoundary>,
+    )
+
+    // error message
+    expect(
+      screen.getByText(/there is an unexpected error/i),
+    ).toBeInTheDocument()
+
+    // reload button
+    expect(screen.getByRole('button', {name: /reload/i})).toBeInTheDocument()
+  })
 })
 
 describe('when the user clicks on reload button', () => {
-  it('must reload the app', () => {})
+  // test to validate app reloads
+  it('must reload the app', () => {
+    // clear app
+    delete window.location
+    // reload mock function
+    window.location = {reload: jest.fn()}
+
+    // render component
+    render(
+      <ErrorBoundary>
+        <ThrowError />
+      </ErrorBoundary>,
+    )
+
+    // event
+    fireEvent.click(screen.getByRole('button', {name: /reload/i}))
+
+    // reload app
+    expect(window.location.reload).toHaveBeenCalledTimes(1)
+  })
 })
