9ea38f1ce4f52ef1d758957320650130b52b4b7a
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-08 19:14:51 +0100

    Add test to validate pagination

--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -356,7 +356,7 @@ describe('when the developer does a search and selects 50 rows per page', () => 
 })
 
 describe('when the developer clicks on search and then on next page button', () => {
-  // control pagination
+  // test to validate pagination
   it('must display the next repositories page', async () => {
     // config server handler
     server.use(rest.get('/search/repositories', handlerPaginated))
@@ -389,5 +389,57 @@ describe('when the developer clicks on search and then on next page button', () 
 
     // expect first repo name is from page 1
     expect(screen.getByRole('cell', {name: /2-0/i})).toBeInTheDocument()
-  }, 10000)
+  }, 30000)
+})
+
+describe('when the developer clicks on search and then on next page button', () => {
+  // test to validate pagination
+  it('must display the next repositories page', async () => {
+    // config server handler
+    server.use(rest.get('/search/repositories', handlerPaginated))
+
+    // click search
+    fireClickSearch()
+
+    // wait table
+    expect(await screen.findByRole('table')).toBeInTheDocument()
+
+    // expect first repo name is from page 0
+    expect(screen.getByRole('cell', {name: /1-0/i})).toBeInTheDocument()
+
+    // expect next page is not disabled
+    expect(screen.getByRole('button', {name: /next page/i})).not.toBeDisabled()
+
+    // click next page button
+    fireEvent.click(screen.getByRole('button', {name: /next page/i}))
+
+    // wait search button is not disabled
+    expect(screen.getByRole('button', {name: /search/i})).toBeDisabled()
+
+    await waitFor(
+      () =>
+        expect(
+          screen.getByRole('button', {name: /search/i}),
+        ).not.toBeDisabled(),
+      {timeout: 3000},
+    )
+
+    // expect first repo name is from page 1
+    expect(screen.getByRole('cell', {name: /2-0/i})).toBeInTheDocument()
+
+    // click on previous page
+    fireEvent.click(screen.getByRole('button', {name: /previous page/i}))
+
+    // wait search finish
+    await waitFor(
+      () =>
+        expect(
+          screen.getByRole('button', {name: /search/i}),
+        ).not.toBeDisabled(),
+      {timeout: 3000},
+    )
+
+    // expect
+    expect(screen.getByRole('cell', {name: /1-0/i})).toBeInTheDocument()
+  }, 30000)
 })
