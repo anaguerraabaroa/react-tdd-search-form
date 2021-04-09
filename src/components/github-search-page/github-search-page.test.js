52b5f23c0e6afd182759a09891ccbe8b99723fc6
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-04-09 14:26:14 +0100

    Migrate integration test to a new file

--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -16,7 +16,6 @@ import {
   getReposListBy,
   makeFakeError,
 } from '../../__fixtures__/repos'
-import {handlerPaginated} from '../../__fixtures__/handlers'
 import {
   OK_STATUS,
   UNEXPECTED_STATUS,
@@ -323,93 +322,6 @@ describe('when the developer types on filter by and does a search', () => {
     // array elements
     expect(repository).toHaveTextContent(expectedRepo.name)
   })
-})
-
-describe('when the developer does a search and selects 50 rows per page', () => {
-  // test to validate rows per page
-  it('must fetch a new search and display 50 rows results on the table', async () => {
-    // config mock server response
-    server.use(rest.get('/search/repositories', handlerPaginated))
-
-    // click search
-    fireClickSearch()
-
-    // wait for table to exist and expect 31 rows per page (30 + table headings)
-    expect(await screen.findByRole('table')).toBeInTheDocument()
-    expect(await screen.findAllByRole('row')).toHaveLength(31)
-
-    // display select options (30,50,100) after click on arrow
-    fireEvent.mouseDown(screen.getByLabelText(/rows per page/i))
-
-    // select 50 rows per page
-    fireEvent.click(screen.getByRole('option', {name: '50'}))
-
-    // wait for the button to not be disabled to render the table
-    // default timeout is 1000 but it is not enough time to complete the test
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', {name: /search/i}),
-        ).not.toBeDisabled(),
-      {timeout: 3000},
-    )
-
-    // expect 51 rows per page (50 + table headings)
-    // default timeout is not enough time to complete the test
-    expect(screen.getAllByRole('row')).toHaveLength(51)
-  }, 6000)
-})
-
-describe('when the developer clicks on search and then on next page button and clicks on search again', () => {
-  // test to validate pagination
-  it.skip('must display the next repositories page', async () => {
-    // config server handler
-    server.use(rest.get('/search/repositories', handlerPaginated))
-
-    // click search
-    fireClickSearch()
-
-    // wait table
-    expect(await screen.findByRole('table')).toBeInTheDocument()
-
-    // expect first repo name is from page 0
-    expect(screen.getByRole('cell', {name: /1-0/i})).toBeInTheDocument()
-
-    // expect next page is not disabled
-    expect(screen.getByRole('button', {name: /next page/i})).not.toBeDisabled()
-
-    // click next page button
-    fireEvent.click(screen.getByRole('button', {name: /next page/i}))
-
-    // wait search button is not disabled
-    expect(screen.getByRole('button', {name: /search/i})).toBeDisabled()
-
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', {name: /search/i}),
-        ).not.toBeDisabled(),
-      {timeout: 3000},
-    )
-
-    // expect first repo name is from page 1
-    expect(screen.getByRole('cell', {name: /2-0/i})).toBeInTheDocument()
-
-    // click on previous page
-    fireEvent.click(screen.getByRole('button', {name: /previous page/i}))
-
-    // wait search finish
-    await waitFor(
-      () =>
-        expect(
-          screen.getByRole('button', {name: /search/i}),
-        ).not.toBeDisabled(),
-      {timeout: 3000},
-    )
-
-    // expect
-    expect(screen.getByRole('cell', {name: /1-0/i})).toBeInTheDocument()
-  }, 30000)
 })
 
 describe('when there is an unprocessable entity from the backend', () => {
--- src/consts/index.js
+++ src/consts/index.js
@@ -1,4 +1,4 @@
-// magic number variables
+// server response variables
 export const OK_STATUS = 200
 export const UNEXPECTED_STATUS = 500
 export const UNPROCESSABLE_STATUS = 422
